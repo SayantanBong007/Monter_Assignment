5552a877ba0158d76b44f07842b9784bd1583f6d
Sayantan Gain <sayantangain63@gmail.com>
2024-04-18 22:05:12 +0530

    Add constants folder

---
+++ constants/reportsData.js
@@ -0,0 +1,17 @@
+export const reportsData = [
+  {
+    date: "22.07.2021",
+    reportName: "APP_URL_USAGE_(Exclude_Half_Days)_3_6_2021_3_31_2021.csv",
+    time: "16:02 PM",
+  },
+  {
+    date: "20.07.2021",
+    reportName: "RUR_L1_01_01_2021_31_03_2021.csv",
+    time: "17:00 PM",
+  },
+  {
+    date: "19.07.2021",
+    reportName: "RUR_L2_(Exclude_Nonworking_Days)_4_1_2021.csv",
+    time: "15:00 PM",
+  },
+];
